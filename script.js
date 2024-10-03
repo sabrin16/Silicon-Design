22ea19ab166e51993fff0f15858a99684d8ec260
Sabrin16 <sabrin.salem@hotmail.se>
2024-10-03 17:04:45 +0200

    har lagt till texter och färger och js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const menuBtn = document.querySelector('.menu-btn')
 const mainMenu = document.querySelector('#main-menu')
 
+const darkmodeSwitch = document.querySelector('#darkmode-switch')
+const hasSetDarkMode = localStorage.getItem('darkmode')
+
 menuBtn.addEventListener('click',() => {
   const isExpanded = menuBtn.getAttribute('aria-expanded')=== 'true'
 
@@ -15,3 +18,37 @@ else {
     menuBtn.setAttribute('aria-expanded',true)
 }
 })
+
+
+if(hasSetDarkMode == null) {
+  if(window.matchMedia('(prefers-color-schema: dark)').matches){
+    enableDarkMode()
+  } else {
+    disableDarkMode()
+  }
+  } else if (hasSetDarkMode == 'on') {
+    enableDarkMode()
+  } else if (hasSetDarkMode == 'off') {
+    disableDarkMode()
+  }
+
+  darkmodeSwitch.addEventListener('change', () => {
+    if(darkmodeSwitch.checked) {
+      document.documentElement.classList.add('dark')
+      localStorage.setItem('darkmode', 'on')
+    }
+    else {
+      document.documentElement.classList.remove('dark')
+      localStorage.setItem('darkmode', 'off')
+    }
+  })
+
+  function enableDarkMode() {
+    darkmodeSwitch.checked = true 
+    document.documentElement.classList.add('dark')
+  }
+  function disableDarkMode() {
+    darkmodeSwitch.checked = false
+    document.documentElement.classList.remove('dark')
+  }
+
