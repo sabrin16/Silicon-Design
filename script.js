dcf0e85eb23081c110baf2a5a4bf1be4fdcaf0b1
Sabrin16 <sabrin.salem@hotmail.se>
2024-10-06 00:19:26 +0200

    Most parts done

--- script.js
+++ script.js
@@ -1,54 +1,52 @@
 const menuBtn = document.querySelector('.menu-btn')
 const mainMenu = document.querySelector('#main-menu')
-
-const darkmodeSwitch = document.querySelector('#darkmode-switch')
+const darkModeSwitch = document.querySelector('#darkmode-switch')
 const hasSetDarkMode = localStorage.getItem('darkmode')
 
-menuBtn.addEventListener('click',() => {
-  const isExpanded = menuBtn.getAttribute('aria-expanded')=== 'true'
-
-if(isExpanded) {
-    menuBtn.setAttribute('aria-expanded', false)
-    mainMenu.addEventListener('animationend', () => {
-      mainMenu.classList.add('hide')
-    }, {once: true})
-}
-else {
-    mainMenu.setAttribute('hide')
-    menuBtn.setAttribute('aria-expanded',true)
-}
+menuBtn.addEventListener('click', () => {
+    const isExpanded = menuBtn.getAttribute('aria-expanded') === 'true'
+    
+    if(isExpanded){
+        menuBtn.setAttribute('aria-expanded', false)
+        mainMenu.addEventListener('animationend', () => {
+            mainMenu.classList.add('hide')
+        }, {once: true})
+    }else{
+        mainMenu.classList.remove('hide')
+        menuBtn.setAttribute('aria-expanded', true);
+    }
 })
 
-
-if(hasSetDarkMode == null) {
-  if(window.matchMedia('(prefers-color-schema: dark)').matches){
-    enableDarkMode()
-  } else {
-    disableDarkMode()
-  }
-  } else if (hasSetDarkMode == 'on') {
-    enableDarkMode()
-  } else if (hasSetDarkMode == 'off') {
-    disableDarkMode()
-  }
-
-  darkmodeSwitch.addEventListener('change', () => {
-    if(darkmodeSwitch.checked) {
-      document.documentElement.classList.add('dark')
-      localStorage.setItem('darkmode', 'on')
+if(hasSetDarkMode == null){
+    if(window.matchMedia('(preferes-color-schem): dark').matches){
+        enableDarkMode()
+    }else{
+        disableDarkMode()
     }
-    else {
-      document.documentElement.classList.remove('dark')
-      localStorage.setItem('darkmode', 'off')
-    }
-  })
+}else if(hasSetDarkMode == 'on'){
+    enableDarkMode()
+}else if(hasSetDarkMode == 'off'){
+    disableDarkMode()
+}
 
-  function enableDarkMode() {
-    darkmodeSwitch.checked = true 
+darkModeSwitch.addEventListener('change', () => {
+    if(darkModeSwitch.checked){
+        document.documentElement.classList.add('dark')
+        localStorage.setItem('darkMode', 'on')
+    }else{
+        document.documentElement.classList.remove('dark')
+        localStorage.setItem('darkMode', 'off')
+    }
+})
+
+function enableDarkMode(){
+    darkModeSwitch.checked = true
     document.documentElement.classList.add('dark')
-  }
-  function disableDarkMode() {
-    darkmodeSwitch.checked = false
+}
+function disableDarkMode(){
+    darkModeSwitch.checked = false
     document.documentElement.classList.remove('dark')
-  }
+}
+
+
 
